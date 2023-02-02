24377501c2fd96cf00a224fcfda5c99c6560c752
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-02-02 01:19:46 -0300

    Exercicios

---
+++ Lista_Exercicios/Ex002.py
@@ -0,0 +1,74 @@
+# 2 - Faça um algoritmo que leia o nome e as notas dos 4 bimestres de um aluno. Posteriormente imprima o resultado de cada variável linha abaixo de linha.
+
+print ("=" * 40)
+
+print ("***Escola Tarda Mais Não Falha***")
+print ("***Relatorio De Notas Bimestrais!***")
+    # Apresentação Ao Usuário.
+
+print ("=" * 40)
+
+nome_aluno = input("Digite o Nome Completo Do Aluno: ")
+    # Nome Da Variável = nome_aluno.
+    # Tipo Da Variável = Str (String).
+    # Função = Entrada De Dados.
+
+print ("=" * 40)
+
+print ("Aluno ({})".format(nome_aluno))
+    # Função = Saída De Dados.
+    # Apresentando Mensagem Na Tela.
+
+print ("-" * 40)
+
+nota_primeiro = float(input("Digite a Nota Do Primeiro Bimestre: "))
+    # Nome Da Variável = nota_primeiro.
+    # Tipo Da Variável = Float (Real).
+    # Função = Entrada De Dados.
+
+nota_segundo = float(input("Digite a Nota Do Segundo Bimestre: "))
+    # Nome Da Variável = nota_segundo.
+    # Tipo Da Variável = Float (Real).
+    # Função = Entrada De Dados.
+
+nota_terceiro = float(input("Digite a Nota Do Terceiro Bimestre: "))
+    # Nome Da Variável = nota_terceiro.
+    # Tipo Da Variável = Float (Real).
+    # Função = Entrada De Dados.
+
+nota_quarto = float(input("Digite a Nota Do Quarto Bimestre: "))
+    # Nome Da Variável = nota_quarto.
+    # Tipo Da Variável = Float (Real).
+    # Função = Entrada De Dados.
+
+print ("=" * 40)
+
+print ("Aluno ({})".format(nome_aluno))
+    # Função = Saída De Dados.
+    # Apresentando Mensagem Na Tela.
+
+
+print ("* Nota 1° Bimestre: {}".format(nota_primeiro))
+    # Função = Saída De Dados.
+    # Apresentando Mensagem Na Tela.
+
+print ("* Nota 2° Bimestre: {}".format(nota_segundo))
+    # Função = Saída De Dados.
+    # Apresentando Mensagem Na Tela.
+
+print ("* Nota 3° Bimestre: {}".format(nota_terceiro))
+    # Função = Saída De Dados.
+    # Apresentando Mensagem Na Tela.
+
+
+print ("* Nota 4° Bimestre: {}".format(nota_quarto))
+    # Função = Saída De Dados.
+    # Apresentando Mensagem Na Tela.
+
+print ("Relatório Finalizado Com Sucesso!")
+    # Função = Saída De Dados.
+    # Apresentando Mensagem Na Tela.
+
+print ("=" * 40)
+
+
