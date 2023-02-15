c2265f052f8c89f2ed613061e81e893ce7c6df03
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-02-15 15:29:38 -0300

    ex

--- Lista_Exercicios/Ex035.py
+++ Lista_Exercicios/Ex035.py
@@ -11,3 +11,128 @@
 # comprada pelo usuário e gere um cupom fiscal, contendo as informações da compra: 
 
 # tipo e quantidade de carne, preço total, tipo de pagamento, valor do desconto e valor a pagar.
+
+
+tipo_carne = input("Qual tipo de carne voce irá querer: ")
+
+# =====================================================================================================================================
+
+if (tipo_carne == "Filé Duplo") or (tipo_carne == "filé Duplo") or (tipo_carne == "file duplo") or (tipo_carne == "File Duplo"):
+
+    kg = float(input("Quantos kilos irá querer: "))
+
+    if (kg <= 5):
+
+        peso = kg * 34.90
+
+        cartao = input("A compra será feita através do cartao da loja [S/N]: ")
+
+        if (cartao == "Sim") or (cartao == "sim") or (cartao == "s") or (cartao == "S"):
+
+            desconto = peso * 5 / 100
+
+            print ("O Valor Final a Ser Pago Será De: R${} ".format(desconto))
+
+        elif (cartao == "Não") or (cartao == "nao") or (cartao == "não") or (cartao == "Nao"):
+
+            print ("O Valor Final a Ser Pago é De: R${}".format(peso))
+
+    # ===========================================================================================
+
+    elif (kg > 5):
+
+        peso = kg * 35.80
+
+        cartao = input("A compra será feita através do cartao da loja [S/N]")
+
+        if (cartao == "Sim") or (cartao == "sim") or (cartao == "s") or (cartao == "S"):
+
+            desconto = peso * 5 / 100
+
+            print ("O valor FInal é de: R${} ".format(desconto))
+            
+        elif (cartao == "Não") or (cartao == "nao") or (cartao == "não") or (cartao == "Nao"):
+
+            print ("O valor Final é De: R${} ".format(peso))
+
+# ========================================================================================================================================
+
+elif (tipo_carne == "Alcatra") or (tipo_carne == "alcatra") or (tipo_carne == "ALCATRA"):
+
+    kg = float(input("Quantos kilos irá querer: "))
+
+    if (kg <= 5):
+
+        peso = kg * 44.90
+
+        cartao = input("A compra será feita através do cartao da loja [S/N]")
+
+        if (cartao == "Sim") or (cartao == "sim") or (cartao == "s") or (cartao == "S"):
+
+            desconto = peso * 5 / 100
+
+            print ("O valor FInal é de: R${} ".format(desconto))
+
+        elif (cartao == "Não") or (cartao == "nao") or (cartao == "não") or (cartao == "Nao"):
+
+            print ("O valor Final é De: R${} ".format(peso))
+
+    # ===========================================================================================
+
+    elif (kg > 5):
+
+        peso = kg * 46.80
+
+        cartao = input("A compra será feita através do cartao da loja [S/N]")
+
+        if (cartao == "Sim") or (cartao == "sim") or (cartao == "s") or (cartao == "S"):
+
+            desconto = peso * 5 / 100
+
+            print ("O valor FInal é de: R${} ".format(desconto))
+
+        elif (cartao == "Não") or (cartao == "nao") or (cartao == "não") or (cartao == "Nao"):
+
+            print ("O valor Final é De: R${} ".format(peso))
+
+# ========================================================================================================================================
+
+elif (tipo_carne == "Picanha") or (tipo_carne == "picanha") or (tipo_carne == "PICANHA"):
+
+    kg = float(input("Quantos kilos irá querer: "))
+
+    if (kg <= 5):
+
+        peso = kg * 66.90
+
+        cartao = input("A compra será feita através do cartao da loja [S/N]: ")
+
+        if (cartao == "Sim") or (cartao == "sim") or (cartao == "s") or (cartao == "S"):
+
+            desconto = peso * 5 / 100
+
+            print ("O valor FInal é de: R${} ".format(desconto))
+
+        elif (cartao == "Não") or (cartao == "nao") or (cartao == "não") or (cartao == "Nao"):
+
+            print ("O valor Final é De: R${} ".format(peso))
+
+    # ===========================================================================================
+
+    elif (kg > 5):
+
+        peso = kg * 67.80
+
+        cartao = input("A compra será feita através do cartao da loja [S/N]")
+
+        if (cartao == "Sim") or (cartao == "sim") or (cartao == "s") or (cartao == "S"):
+
+            desconto = peso * 5 / 100
+
+            print ("O valor FInal é de: R${} ".format(desconto))
+
+        elif (cartao == "Não") or (cartao == "nao") or (cartao == "não") or (cartao == "Nao"):
+
+            print ("O valor Final é De: R${} ".format(peso))
+
+# ========================================================================================================================================
--- Lista_Exercicios/Ex036.py
+++ Lista_Exercicios/Ex036.py
@@ -13,3 +13,64 @@
 # Escreva um algoritmo que leia o código de um sanduíche e de uma bebida, e mostre o valor a pagar pelo cliente.
 
 # Assumaas entradas corretas:
+
+cachorro_quente = 11.20
+ovo_simples = 8.30
+bauru_com_ovo = 11.50
+hamburguer = 16.20
+refrigerante = 6.0
+suco = 7.50
+agua_mineral = 4.70
+carrinho = 0
+
+print ("Cachorro Quente --> Código Do Produto: [100]")
+print ("Ovo Simples     --> Código Do Produto: [101]")
+print ("Bauru Com Ovo   --> Código Do Produto: [102]")
+print ("Hamburger       --> Código Do Produto: [103]")
+print ("Refrigerante    --> Código Do Produto: [201]")
+print ("Suco            --> Código Do Produto: [202]")
+print ("Agua Mineral    --> Código Do Produto: [203]")
+
+resposta = input("Deseja fazer o Pedido [S/N]: ")
+
+while (resposta == "Sim") or (resposta == "sim"):    
+
+    
+    codigo = input("Digite o código do produto: ")
+
+    if (codigo == "100"):
+
+        carrinho = carrinho + cachorro_quente
+        print ("+ 1 cachorro quente")
+
+    if (codigo == "101"):
+
+        carrinho = carrinho + ovo_simples
+        print ("+ 1 Ovo Simples")
+
+    if (codigo == "102"):
+
+        carrinho = carrinho + bauru_com_ovo
+        print ("+ 1 Bauru Com Ovo")
+
+    if (codigo == "103"):
+
+        carrinho = carrinho + hamburguer
+        print ("+ 1 Hamburger")
+
+    if (codigo == "201"):
+
+        carrinho = carrinho + refrigerante
+        print ("+ 1 Refrigerante")
+
+    if (codigo == "202"):
+
+        carrinho = carrinho + suco
+        print ("+ 1 Suco")
+
+    if (codigo == "203"):
+
+        carrinho = carrinho + agua_mineral
+        print ("+ 1 Agua Mineral")
+
+    resposta = input("Deseja continuar o Pedido [S/N]: ")
