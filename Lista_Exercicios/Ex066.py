488f4d347ad91df6ebde3d7c1316fbbc41002e77
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-02-19 16:02:20 -0300

    Lista_Exercicios

--- Lista_Exercicios/Ex066.py
+++ Lista_Exercicios/Ex066.py
@@ -3,10 +3,93 @@
 # iguale a população do país B, mantidas as taxas de crescimento.
 
 
+pais_a = 80000
+
+pais_b = 200000
+
+crescimento_a = pais_a * 3 /100
+
+crescimento_b = pais_b * 1.5 / 100
+
+taxa_anual_p_a = crescimento_a
+
+taxa_anual_p_b = crescimento_b
+
+
+while (pais_b > pais_a):
+
+    print ("")
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""primeiro_ano_a = pais_a + taxa_anual_p_a
+primeiro_ano_b = pais_b + taxa_anual_p_b
+
+segundo_ano_a = pais_a + taxa_anual_p_a + primeiro_ano_a
+segundo_ano_b = pais_b + taxa_anual_p_b + primeiro_ano_b
+
+terceiro_ano_a = pais_a + taxa_anual_p_a + primeiro_ano_a + segundo_ano_a
+terceiro_ano_b = pais_b + taxa_anual_p_b + primeiro_ano_b + segundo_ano_b
+
+quarto_ano_a = pais_a + taxa_anual_p_a + primeiro_ano_a + segundo_ano_a + terceiro_ano_a
+quarto_ano_b = pais_b + taxa_anual_p_b + primeiro_ano_b + segundo_ano_b + terceiro_ano_b
+
+quinto_ano_a = pais_a + taxa_anual_p_a + primeiro_ano_a + segundo_ano_a + terceiro_ano_a + quarto_ano_a
+quinto_ano_b = pais_b + taxa_anual_p_b + primeiro_ano_b + segundo_ano_b + terceiro_ano_b + quarto_ano_b
+
+print (primeiro_ano_a)
+print (primeiro_ano_b)
+
+print ("====")
+
+print (segundo_ano_a)
+print (segundo_ano_b)
+
+print ("====")
+
+print (terceiro_ano_a)
+print (terceiro_ano_b)
+
+print ("====")
+
+print (quarto_ano_a)
+print (quarto_ano_b)
+
+print ("====")
+
+print (quinto_ano_a)
+print (quinto_ano_b)"""
 
 
 
