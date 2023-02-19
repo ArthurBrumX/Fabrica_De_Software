432761381aa2012ee16ef3343b8ec9a10dc3d01d
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-02-19 02:21:51 -0300

    Lista_Exercicios

--- Lista_Exercicios/Ex060.py
+++ Lista_Exercicios/Ex060.py
@@ -18,68 +18,68 @@ print ("*** Sejá Bem-Vindo! ***")
 
 print ("=" * 60)
 
-numero = int(input("Digite Um Número Para Ver a Taboada: "))
+numero = int(input("* Digite Um Número Para Ver a Taboada: "))
     # Nome Da Variável = numero.
     # Tipo Da Variável = Int (Inteiro).
     # Função = Entrada De Dados.
 
 print ("=" * 60)
 
-print ("1 X {} == {}".format(numero, numero * 1))
+print ("- 1 X {} == {}".format(numero, numero * 1))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
         # Ex: .format(Nome_da_Variável).
 
-print ("2 X {} == {}".format(numero, numero * 2))
+print ("- 2 X {} == {}".format(numero, numero * 2))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
         # Ex: .format(Nome_da_Variável).
 
-print ("3 X {} == {}".format(numero, numero * 3))
+print ("- 3 X {} == {}".format(numero, numero * 3))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
         # Ex: .format(Nome_da_Variável).
 
-print ("4 X {} == {}".format(numero, numero * 4))
+print ("- 4 X {} == {}".format(numero, numero * 4))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
         # Ex: .format(Nome_da_Variável).
 
-print ("5 X {} == {}".format(numero, numero * 5))
+print ("- 5 X {} == {}".format(numero, numero * 5))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
         # Ex: .format(Nome_da_Variável).
 
-print ("6 X {} == {}".format(numero, numero * 6))
+print ("- 6 X {} == {}".format(numero, numero * 6))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
         # Ex: .format(Nome_da_Variável).
 
-print ("7 X {} == {}".format(numero, numero * 7))
+print ("- 7 X {} == {}".format(numero, numero * 7))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
         # Ex: .format(Nome_da_Variável).
 
-print ("8 X {} == {}".format(numero, numero * 8))
+print ("- 8 X {} == {}".format(numero, numero * 8))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
         # Ex: .format(Nome_da_Variável).
 
-print ("9 X {} == {}".format(numero, numero * 9))
+print ("- 9 X {} == {}".format(numero, numero * 9))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
         # Ex: .format(Nome_da_Variável).
 
-print ("10 X {} == {}".format(numero, numero * 10))
+print ("- 10 X {} == {}".format(numero, numero * 10))
     # Função = Saída De Dados.
     # Apresentando Mensagem Na Tela.
     # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
