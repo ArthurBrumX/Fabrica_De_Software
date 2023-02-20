aaff8d2594ec218bfb9ce970fd63e68c74cb46f3
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-02-20 14:39:34 -0300

    Lista_Exercicios

--- Lista_Exercicios/Ex066.py
+++ Lista_Exercicios/Ex066.py
@@ -2,94 +2,89 @@
 # taxa de crescimento de 1.5%. Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou 
 # iguale a população do país B, mantidas as taxas de crescimento.
 
+print ("=" * 60)
+
+print ("*** Taxa Anual De Crescimento! ***")
+    # Boas-Vindas Ao Usuario.
+
+print ("*** Sejá Bem-Vindo! ***")
+    # Apresentação Ao Usuário.
+
+print ("=" * 60)
+
+contador = 0
+    # Nome Da Variável = contador.
+    # Tipo Da Variavel = Int (Inteiro).
+    # Função = Atribuição De Valor.
+
+anos = 0 
+    # Nome Da Variável = anos.
+    # Tipo Da Variavel = Int (Inteiro).
+    # Função = Atribuição De Valor.
 
 pais_a = 80000
+    # Nome Da Variável = pais_a.
+    # Tipo Da Variavel = Int (Inteiro).
+    # Função = Atribuição De Valor.
 
 pais_b = 200000
+    # Nome Da Variável = pais_b.
+    # Tipo Da Variavel = Int (Inteiro).
+    # Função = Atribuição De Valor.
 
 crescimento_a = pais_a * 3 /100
+    # Nome Da Variável = crescimento_a.
+    # Função = Calculo Com Variáveis
+    # Calculo: (Valor da Variável: ) -> pais_a * 3 / 100
 
 crescimento_b = pais_b * 1.5 / 100
+    # Nome Da Variável = crescimento_b.
+    # Função = Calculo Com Variáveis
+    # Calculo: (Valor da Variável: ) -> pais_b * 1.5 / 100
 
 taxa_anual_p_a = crescimento_a
+    # Nome Da Variável = taxa_anual_p_a.
+    # Tipo Da Variavel = Int (Inteiro).
+    # Função = Atribuição De Valor.
 
 taxa_anual_p_b = crescimento_b
-
+    # Nome Da Variável = taxa_anual_p_b.
+    # Tipo Da Variavel = Int (Inteiro).
+    # Função = Atribuição De Valor.
 
 while (pais_b > pais_a):
+    # While = Enquanto
+        # Enquanto Essa Condição For Verdadeira Execute o Código a Baixo.
 
-    print ("")
+    contador = contador + 1
+        # Nome Da Variável = contador.
+        # Função = Calculo Com Variáveis
+        # Calculo: (Valor da Variável: ) -> contador + 1
 
+    anos = anos + 1
+        # Nome Da Variável = crescimento_a.
+        # Função = Calculo Com Variáveis
+        # Calculo: (Valor da Variável: ) -> anos + 1
 
+    pais_a = pais_a + crescimento_a + taxa_anual_p_a
+        # Nome Da Variável = pais_a.
+        # Função = Calculo Com Variáveis
+        # Calculo: (Valor da Variável: ) -> pais_a + (Valor da Variável: ) -> crescimento_a + (Valor da Variável: ) -> taxa_anual_p_a
 
+    print ("- No {}° ano seram {} habitantes!!".format(contador, pais_a))
+        # Função = Saída De Dados.
+        # Apresentando Mensagem Na Tela.
+        # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
+            # Ex: .format(Nome_da_Variável).
 
+print ("=" * 60)
 
+print ("- Seram precisos {} anos para se igualar ao pais B!! ".format(anos))
+    # Função = Saída De Dados.
+    # Apresentando Mensagem Na Tela.
+    # .format = Formata o Texto, Alocando o Valor De Uma Variável No Espaço: {}.
+        # Ex: .format(Nome_da_Variável).
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""primeiro_ano_a = pais_a + taxa_anual_p_a
-primeiro_ano_b = pais_b + taxa_anual_p_b
-
-segundo_ano_a = pais_a + taxa_anual_p_a + primeiro_ano_a
-segundo_ano_b = pais_b + taxa_anual_p_b + primeiro_ano_b
-
-terceiro_ano_a = pais_a + taxa_anual_p_a + primeiro_ano_a + segundo_ano_a
-terceiro_ano_b = pais_b + taxa_anual_p_b + primeiro_ano_b + segundo_ano_b
-
-quarto_ano_a = pais_a + taxa_anual_p_a + primeiro_ano_a + segundo_ano_a + terceiro_ano_a
-quarto_ano_b = pais_b + taxa_anual_p_b + primeiro_ano_b + segundo_ano_b + terceiro_ano_b
-
-quinto_ano_a = pais_a + taxa_anual_p_a + primeiro_ano_a + segundo_ano_a + terceiro_ano_a + quarto_ano_a
-quinto_ano_b = pais_b + taxa_anual_p_b + primeiro_ano_b + segundo_ano_b + terceiro_ano_b + quarto_ano_b
-
-print (primeiro_ano_a)
-print (primeiro_ano_b)
-
-print ("====")
-
-print (segundo_ano_a)
-print (segundo_ano_b)
-
-print ("====")
-
-print (terceiro_ano_a)
-print (terceiro_ano_b)
-
-print ("====")
-
-print (quarto_ano_a)
-print (quarto_ano_b)
-
-print ("====")
-
-print (quinto_ano_a)
-print (quinto_ano_b)"""
-
+print ("=" * 60)
 
 
--- Lista_Exercicios/Ex070.py
+++ Lista_Exercicios/Ex070.py
@@ -15,6 +15,123 @@
 # Melhor salto: 6.5 m
 # Pior salto: 5.3 m
 
+contador = 0
+
+melhor_salto = 0
+
+pior_salto = 0 
+
+cont_salto = 0
+
+primeiro = 0
+
+segundo = 0
+
+terceiro = 0
+
+quarto = 0
+
+quinto = 0
+
+nome = input("- Informe o Nome Do Atleta: ")
+
+while (cont_salto < 5):
+
+    cont_salto = cont_salto + 1
+
+    salto = float(input("- Informe a Distancia Do Salto: "))
+
+    contador = contador + 1
+
+    if (contador == 1):
+
+        melhor_salto = salto
+
+        pior_salto = salto
+
+    else:
+
+        if (salto > melhor_salto):
+
+            melhor_salto = salto
+        
+        if (salto < pior_salto):
+
+            pior_salto = salto
+
+media = segundo + terceiro + quarto / 5
+
+print ("- Melhor Salto: {}".format(melhor_salto))
+
+print ("- Pior Salto: {}".format(pior_salto))
+
+print (salto)
+
+print (media)
+
+print (contador)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""nome = input("- Informe o Nome Do Atleta: ")
+
+primeiro = float(input("- Informe a Distancia Do Primeiro Salto: "))
+segundo = float(input("- Informe a Distancia Do Segundo Salto: "))
+terceiro = float(input("- Informe a Distancia Do Terceiro Salto: "))
+quarto = float(input("- Informe a Distancia Do Quarto Salto: "))
+quinto = float(input("- Informe a Distancia Do Quinto Salto: "))
+
+media = segundo + terceiro + quarto / 5"""
 
 
 
