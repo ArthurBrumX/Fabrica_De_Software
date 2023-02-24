d9afed62983454d0db5283ecd7b443122f430969
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-02-24 00:58:28 -0300

    Lista_Exercicios

--- Lista_Exercicios/Ex078.py
+++ Lista_Exercicios/Ex078.py
@@ -1,1 +1,28 @@
 # 78 - Faça um Programa que leia 20 números inteiros e armazene-os num vetor. Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprimaos três vetores.
+
+numero = []
+
+par = []
+
+impar =[]
+
+for c in range (0, 5):
+
+    numeros = int(input("Digite Um numero Inteiro:"))
+
+    numero.append(numeros)
+
+
+    maximo= max(clientes)
+    minimo = min(clientes)
+
+    ma = clientes.index(maximo)
+    mi = clientes.index(minimo)
+
+
+
+
+
+
+
+
