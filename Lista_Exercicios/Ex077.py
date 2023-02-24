8781b00d42ba65b56a755427d56baab97a7fb06b
Arthur Brum Xavier <98411287+ArthurBrumX@users.noreply.github.com>
2023-02-24 00:11:03 -0300

    Update Ex077.py

--- Lista_Exercicios/Ex077.py
+++ Lista_Exercicios/Ex077.py
@@ -1,17 +1,12 @@
 # 77 - Faça um Programa que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
 
-vogais = ["a, e, i, o, u"]
+vogais = ["aeiou"]
 
 caracter = ["cha mate leao"]
 
-caracter.count("a")
-caracter.count("e")
-caracter.count("i")
-caracter.count("o")
-caracter.count("u")
+caracter.count("a") 
+
+print (caracter [0:1])
 
 print (caracter.count("a"))
-print (caracter.count("e"))
-print (caracter.count("i"))
-print (caracter.count("o"))
-print (caracter.count("u"))
+
